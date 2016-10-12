e0681446fd363e8c0cee7fc3923e87567aa95798
frankun <836783712@qq.com>
2016-10-12 16:43:03 +0800

    config

---
+++ src/main/java/com.frankun/service/UserService.java
@@ -0,0 +1,9 @@
+package com.frankun.service;
+
+/**
+ * 2016/10/12
+ *
+ * @author frankun
+ */
+public class UserService {
+}
---
+++ src/main/java/com.frankun/service/impl/OauthServiceImpl.java
@@ -0,0 +1,9 @@
+package com.frankun.service.impl;
+
+/**
+ * 2016/10/12
+ *
+ * @author frankun
+ */
+public class OauthServiceImpl {
+}
