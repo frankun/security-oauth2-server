67656094dcb498800bfa9546c1165d89ca26ed4f
frankun <836783712@qq.com>
2016-10-14 17:29:06 +0800

    mapper

---
+++ src/main/java/com.frankun/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.frankun.dao;
+
+import com.frankun.dao.base.BaseMapper;
+import com.frankun.domain.user.User;
+
+import java.util.List;
+
+/**
+ * 2016/10/14
+ *
+ * @author frankun
+ */
+public interface UserMapper extends BaseMapper{
+
+    User findByGuid(String guid);
+    void saveUser(User user);
+    void updateUser(User user);
+    User findByUsername(String username);
+    List<User> findUserByUsername(String username);
+}
