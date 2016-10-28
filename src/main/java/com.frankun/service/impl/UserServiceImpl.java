bf86619beabeb63ff3715b5bedd6f93f470b8059
frankun <836783712@qq.com>
2016-10-28 16:50:03 +0800

    userService

--- src/main/java/com.frankun/service/impl/UserServiceImpl.java
+++ src/main/java/com.frankun/service/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.frankun.service.impl;
 
+import com.frankun.dao.UserMapper;
+import com.frankun.domain.security.myUserDetails;
+import com.frankun.domain.user.User;
 import com.frankun.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,11 +18,18 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl implements UserService{
 
+    @Autowired
+    private UserMapper userMapper;
+
     public boolean isUsernameExisted(String username){
         return false;
     }
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userMapper.findByUsername(username);
+        if (user == null || user.isArchived()){
+            throw new UsernameNotFoundException("Not found user for this username [" + username + "]");
+        }
+        return new myUserDetails(user);
     }
 }
