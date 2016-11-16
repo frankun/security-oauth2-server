850fbb21d45fb503d474b8951a67f52704e8385a
frankun <836783712@qq.com>
2016-11-16 16:39:31 +0800

    delete changes

--- src/main/java/com.frankun/domain/dto/OauthClientDetailsDto.java
+++
@@ -1,8 +0,0 @@
-package com.frankun.domain.dto;
-
-/**
- * 2016/10/12
- *
- */
-public class OauthClientDetailsDto {
-}
--- src/main/java/com.frankun/web/controller/BaseController.java
+++
@@ -1,7 +0,0 @@
-package com.frankun.web.controller;
-
-/**
- * Created by frankun on 2016/10/10.
- */
-public class BaseController {
-}
