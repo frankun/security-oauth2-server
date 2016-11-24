d727f30d29eabaf143026370c4fd6a3c0bbea1e3
frankun <836783712@qq.com>
2016-11-24 14:56:33 +0800

    show user_form

--- src/main/java/com/frankun/web/controller/UserController.java
+++ src/main/java/com/frankun/web/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.frankun.web.controller;
 
+import com.frankun.domain.dto.UserFormDto;
 import com.frankun.domain.dto.UserOverviewDto;
 import com.frankun.service.UserService;
-import com.frankun.web.validator.UserFormDtoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 2016/11/21
@@ -28,5 +29,11 @@ public class UserController {
         overviewDto = userService.loadUserOverviewDto(overviewDto);
         model.addAttribute("overviewDto", overviewDto);
         return "user_overview";
+    }
+
+    @RequestMapping(value = "form/plus", method = RequestMethod.GET)
+    public String showForm(Model model){
+        model.addAttribute("formDto", new UserFormDto());
+        return "user_form";
     }
 }
